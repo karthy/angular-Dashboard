3a35b062e2310a0564c2cc28a9ef9c6d26e7517a
karthy <karthickted@gmail.com>
2019-10-08 15:48:18 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavDashboardComponent } from './nav-dashboard/nav-dashboard.component';
 import { MaterialModule } from './material/material.module';
 import { DashDetailComponent } from './dash-detail/dash-detail.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MaterialModule],
+  imports:      [ BrowserModule, FormsModule, MaterialModule, RouterModule],
   declarations: [ AppComponent, DashboardComponent, NavDashboardComponent, DashDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
